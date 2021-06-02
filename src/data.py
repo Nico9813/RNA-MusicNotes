6f874ecd11d20abb8b9b3578f26ba63e4672bb72
nico9813 <nico.gomez.mbc@gmail.com>
2021-06-02 15:48:09 -0300

    add comentarios

--- src/data.py
+++ src/data.py
@@ -1,3 +1,22 @@
 import tensorflow as tf
 
 print(tf.__version__)
+
+# Definir parametros globales 
+rna_cant_neuronas_capas_ocultas = '800, 400, 200' #@param {type:"string"}
+rna_tipo_capa_salida = 'softmax-MultiClase' #@param ["lineal-Numero", "softmax-MultiClase"] (???????)
+rna_cant_epocas_entrenamiento =  200#@param {type:"integer"}
+rna_learning_rate_entrenamiento = 0.001 #@param {type:"number"}
+cantEpocas = (100 if rna_cant_epocas_entrenamiento<1 else rna_cant_epocas_entrenamiento)
+
+# Levantar los datos de entrada (entrenamiento, test y validacion)
+#  - Entrada: Archivo .WAV [-1, 1.0 ,0.5]
+
+# Normalizar el tamaño de los datos de entrada
+# Generar diccionario auxiliar para poder convertir de ID de clase a nombre de clase
+
+# Crear el modelo 
+# - Capa de entrada
+# - Capas ocultas
+# - Capa de salida
+
