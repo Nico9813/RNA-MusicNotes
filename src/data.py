d8330aefc6231b20a93e7958a790b2a036a20c85
nico9813 <nico.gomez.mbc@gmail.com>
2021-06-04 05:08:19 +0000

    audio to image CNN

--- src/data.py
+++ src/data.py
@@ -1,77 +1,127 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras import models
+from matplotlib import pyplot as plt
 import numpy as np
+import os
+from tensorflow.keras.layers.experimental import preprocessing
 
 print(tf.__version__)
 
 # Definir rutas datos
-path_data = "data"
-path_data_training = path_data + "/nsynth-test.tfrecord"
-
-# Definir parametros globales 
-rna_cant_neuronas_capas_ocultas = '800, 400, 200' #@param {type:"string"}
-rna_tipo_capa_salida = 'softmax-MultiClase' #@param ["lineal-Numero", "softmax-MultiClase"] (???????)
-rna_cant_epocas_entrenamiento =  200#@param {type:"integer"}
-rna_learning_rate_entrenamiento = 0.001 #@param {type:"number"}
-cantEpocas = (100 if rna_cant_epocas_entrenamiento<1 else rna_cant_epocas_entrenamiento)
+path_data = "data/"
+path_data_training = path_data + "nsynth_test/audio/"
+path_data_test = path_data + "nsynth_test/audio/"
+path_labels_training = path_data + "nsynth_test/examples.json"
 
 # Levantar los datos de entrada (entrenamiento, test y validacion)
 #  - Entrada: Archivo .WAV [-1, 1.0 ,0.5]
 
-def get_data(file):
-    dataset = tf.data.TFRecordDataset([file])
+def get_waveform_and_label(file_path):
+  parts = tf.strings.split(file_path, "-")
+  label = int(parts[1])
+  audio_binary = tf.io.read_file(file_path)
+  audio, _ = tf.audio.decode_wav(audio_binary)
+  waveform = tf.squeeze(audio, axis=1)
+  return waveform, label
 
-    x = []
-    y = []
+def get_spectrogram(waveform):
 
-    i = 0
+  # Concatenate audio with padding so that all audio clips will be of the
+  # same length
+  zero_padding = tf.zeros([64001] - tf.shape(waveform), dtype=tf.float32)
+  # Concatenate audio with padding so that all audio clips will be of the
+  # same length
+  waveform = tf.cast(waveform, tf.float32)
+  equal_length = tf.concat([waveform, zero_padding], 0)
+  spectrogram = tf.signal.stft(
+      equal_length, frame_length=255, frame_step=128)
 
-    for element in dataset:
-        if i < 100:
-            record = tf.train.Example()
-            record.ParseFromString(element.numpy())
+  spectrogram = tf.abs(spectrogram)
 
-            record_input = record.features.feature["audio"].float_list.value
-            record_label = record.features.feature["pitch"].int64_list.value[0]
+  print("waveform " + str(waveform.shape))
+  print("esepctograma " + str(spectrogram.shape))
+  return spectrogram
 
-            x.append(record_input)
-            y.append(record_label)
-            
-            i += 1
-    return np.array(x),np.array(y)
-    
-x_train, y_train = get_data(path_data_training)
+def get_spectrogram_and_label_id(audio, label):
+  spectrogram = get_spectrogram(audio)
+  spectrogram = tf.expand_dims(spectrogram, -1)
+  label_id = label
+  return spectrogram, label_id
+
+def get_data(path_data):
+  train_files = list(map(lambda path: path_data + path, os.listdir(path_data)))
+  files_ds = tf.data.Dataset.from_tensor_slices(train_files)
+  #for element in files_ds.as_numpy_iterator():
+  #  print(element)
+  waveform_ds = files_ds.map(get_waveform_and_label)
+  return waveform_ds.map(get_spectrogram_and_label_id)
+
+ds_train = get_data(path_data_training)
+ds_test = get_data(path_data_test)
+
+batch_size = 64
+ds_train = ds_train.batch(batch_size)
+ds_test = ds_test.batch(batch_size)
+
+ds_train = ds_train.cache().prefetch(tf.data.AUTOTUNE)
+ds_test = ds_test.cache().prefetch(tf.data.AUTOTUNE)
+
+for spectrogram, _ in ds_train.take(1):
+  input_shape = spectrogram.shape
+  
+print('Input shape:', input_shape)
+num_labels = 150
+
+norm_layer = preprocessing.Normalization()
+norm_layer.adapt(ds_train.map(lambda x, _: x))
+
+model = models.Sequential([
+    layers.InputLayer(input_shape=(499, 129, 1)),
+    preprocessing.Resizing(32, 32),
+    norm_layer,
+    layers.Conv2D(32, 3, activation='relu'),
+    layers.Conv2D(64, 3, activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.25),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(num_labels),
+])
+
+model.summary()
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'],
+)
+
+EPOCHS = 10
+history = model.fit(
+    ds_train,
+    validation_data=ds_train,
+    batch_size=64,
+    epochs=EPOCHS,
+    verbose='auto',
+    callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=2),
+)
+
+metrics = history.history
+plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
+plt.legend(['loss', 'val_loss'])
+plt.savefig("loss")
+
+
+
 
 # x_test, y_test = get_data(path_data_test)
 # x_validation, y_validation = get_data(path_data_validation)
 
 # Generar graficos de las entradas
 
-# Normalizar el tamaño de los datos de entrada
+# Normalizar el tamaÃ±o de los datos de entrada
 # Generar diccionario auxiliar para poder convertir de ID de clase a nombre de clase
-
-# Crear el modelo
-# - Capa de entrada
-
-input_layer = layers.Dense(2, activation="relu", name="input_raw_file")
-# hidden_layers = []
-output_layer = layers.Dense(4, name="output_pitch")
-
-model = keras.Sequential([
-    input_layer,
-    output_layer
-])
-
-opt = keras.optimizers.Adam(learning_rate=rna_learning_rate_entrenamiento)
-
-model.compile(optimizer=opt, loss='mse', metrics=['accuracy'])
-
-history = model.fit(x_train, y_train, epochs=cantEpocas)
-
-model.summary()
-
-# - Capas ocultas
-# - Capa de salida
-
 
